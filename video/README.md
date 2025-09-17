c95902894c070597e1a6f8e911a6508504e7608e
YehezkielDarren <yehezkiel.darren@ti.ukdw.ac.id>
2025-09-17 19:51:20 +0700

    Move path for example task

---
+++ video/README.md
@@ -0,0 +1,43 @@
+# Periapan : Pembersihan Data pada Sebuah Teks Berita (Latihan Soal untuk Video)
+
+## Latar Belakang
+
+Sebuah lembaga riset politik ingin mendapatkan gambaran yang lebih mendalam mengenai narasi berita dan respons publik terhadapnya. Mereka memiliki dua set data:
+
+1. Data Berita: Berisi judul dan isi artikel berita.
+
+2. Data Komentar: Berisi komentar-komentar yang diberikan oleh pembaca terhadap artikel berita.
+
+**Tugas Anda** adalah membuat program yang dapat melakukan proses pembersihan data di kolom `komentar` pada `comment_news.csv` dan kolom `headline` di `news_data.csv`.
+
+## Deskripsi Soal
+
+Fokus dari bagian ini adalah melakukan pra-pemrosesan teks untuk mendapatkan teks bersih tanpa tanda baca dan angka di setiap kolom pada kedua file CSV.
+
+- Pemuatan Data Berita: Baca seluruh data dari news_data.csv. Simpan data ini ke dalam dictionary di mana idBerita menjadi kunci dan data berita (Headline, Content) menjadi nilai.
+
+- Pra-pemrosesan Teks: Lakukan langkah-langkah pembersihan kolom Content:
+
+  1. Konversi ke huruf kecil.
+
+  2. Penghapusan tanda baca dan angka.
+
+## Struktur Data File
+
+Ada dua file CSV yang akan Anda gunakan:
+
+1. `news_data.csv`
+
+   - Berisi data berita.
+
+   - Kolom: `idBerita`, `Headline`, `Content`.
+
+   - idBerita berfungsi sebagai Primary Key.
+
+2. `comment_news.csv`
+
+   - Berisi data komentar.
+
+   - Kolom: `idKomentar`, `idBerita`, `Komentar`, `Rating`.
+
+   - `idBerita` di sini berfungsi sebagai Foreign Key, yang menghubungkan setiap komentar ke berita yang relevan.
---
+++ video/scripts/script.py
@@ -0,0 +1,69 @@
+import csv
+import string
+import pandas as pd
+
+
+
+
+def clean_text(data:str) -> list:
+    text = data.lower().rstrip()
+    translator = str.maketrans("","", string.punctuation+string.digits)
+    result = text.translate(translator).split()
+    return result
+    
+def read_comment(filename):
+    data=[]
+    try:
+        with open(filename, mode="r") as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                cleanned_comments = clean_text(row['Komentar'])
+                data.append({
+                    "idKomentar": row["idKomentar"],
+                    "idBerita": row["idBerita"],
+                    "komentar": cleanned_comments,
+                    "rating": row["Rating"]
+                })
+    except FileNotFoundError:
+        return f"Error: File '{filename}' tidak ditemukan."                    
+    return data
+
+def read_news(filename):
+    data=[]
+    try:
+        with open(filename, mode="r") as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                cleanned_content = clean_text(row['Content'])
+                data.append({
+                    "idBerita": row["idBerita"],
+                    "headline": row["Headline"],
+                    "content": cleanned_content
+                })
+    except FileNotFoundError:
+        return f"Error: File '{filename}' tidak ditemukan."  
+    return data
+
+if __name__ == "__main__":
+    # read all datas
+    comment_news = read_comment("./comment_news.csv")
+    news = read_news("./news_data.csv")
+    
+    words_dict = dict()
+    words = [x["komentar"] for x in comment_news] + [y["content"] for y in news]
+    # Flatten the list of lists into a single list of words
+    all_words = []
+    for word_list in words:
+        all_words.extend(word_list)
+    # Count word frequency
+    for word in all_words:
+        if word in words_dict:
+            words_dict[word] += 1
+        else:
+            words_dict[word] = 1
+    # Sort the words_dict by frequency in descending order
+    sorted_words = sorted(words_dict.items(), key=lambda item: item[1], reverse=True)
+    df = pd.DataFrame(sorted_words, columns=['word', 'frequency'])
+    print(df.head(10))
+        
+    
